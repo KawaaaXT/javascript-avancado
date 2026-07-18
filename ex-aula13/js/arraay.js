c550bb17ee38e967c05e7620a8822448c2250f15
KawaaaXT <kdelima8513@gmail.com>
2026-07-18 15:37:07 +0100

    Adiciona exercício de JS sobre métodos de arrays

---
+++ ex-aula13/js/arraay.js
@@ -0,0 +1,16 @@
+const pessoasReais = ['Kaua', 'Gustavo', 'David', 'Ramon']
+console.log(pessoasReais.slice(0,3))
+
+
+
+
+
+
+//pessoasReais.unshift('Italo')
+//pessoasReais.push('Sabrina')
+//const removido = pessoasReais.pop()
+//const removido2 = pessoasReais.shift()
+//console.log(pessoasReais)
+//console.log(removido)
+//console.log(removido2)
+
