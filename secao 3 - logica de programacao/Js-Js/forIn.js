2b8c2698a5ce5b2aa37ff8c123a53352674d790b
KawaaaXT <kdelima8513@gmail.com>
2026-08-10 21:59:54 +0100

    Adiciona , exercicio For in.. de Js

---
+++ secao 3 - logica de programacao/Js-Js/forIn.js
@@ -0,0 +1,56 @@
+
+
+
+const produtos = {
+    teclado: 150,
+    mouse: 90,
+    monitor: 1200,
+    gabinete: 350
+}
+
+for (let chave in produtos){
+    
+}
+
+
+
+
+
+/*
+const notas = {
+    matematica: 8,
+    portugues: 8,
+    geografia: 6,
+    quimica: 7
+
+}
+
+let soma = 0
+
+for (let chave in notas){
+    soma += notas[chave]
+}
+
+console.log(soma)
+
+*/
+
+
+
+
+/*
+const propriedades = {
+    nome: "kaua" ,
+    sobrenome: "Lima",
+    idade: 21,
+    profissao: "progamador",
+    ano: 2026,
+};
+
+let contador = 0
+for (let chave in propriedades  ){
+    contador++
+}
+console.log(contador)
+
+*/
