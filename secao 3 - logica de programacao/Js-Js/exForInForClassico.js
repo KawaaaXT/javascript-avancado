fc91cda17903cff5b762d3b84af33c9e0a094649
KawaaaXT <kdelima8513@gmail.com>
2026-08-11 21:37:24 +0100

    Adiciona exercícios de JavaScript sobre for e for...in

---
+++ secao 3 - logica de programacao/Js-Js/exForInForClassico.js
@@ -0,0 +1,47 @@
+
+
+
+
+const numeros = [3, 8, 12, 5, 20, 7, 15, 2];
+
+for (let i = 0; i < numeros.length ; i++){
+    if(numeros[i] > 10){
+        console.log(`${numeros[i]} é maior que 10`)
+    }else {
+        console.log(`${numeros[i]} não é maior que 10`)
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+const produtos = {
+    nome: "Notebook",
+    preco: 2500,
+    estoque: 10,
+    categoria: "Eletronicos"
+}
+
+for (let chave in produtos){
+    console.log(chave, ":", produtos[chave])
+}
+*/
+
+
+
+
+
+/*
+const nomes = ["Kauã", "Daniel", "Ramon", "Daniela", "Pedro"]
+
+for (let i = 0; i < 5 ; i++ ){
+    console.log(`Nome : ${nomes[i]}`)
+}
+
+*/
