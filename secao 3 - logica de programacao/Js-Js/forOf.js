71a97a9f90bcb8e7b1a7194b7d210d64be1e4d79
KawaaaXT <kdelima8513@gmail.com>
2026-08-12 21:53:15 +0100

    Adiciona exercício sobre for...of em JavaScript

---
+++ secao 3 - logica de programacao/Js-Js/forOf.js
@@ -0,0 +1,25 @@
+
+
+
+const numeros = [20,30,40,50,70]
+
+let soma = 0
+
+for (let numero of numeros){
+    soma += numero
+}
+console.log(`A Soma deu  : ${soma}`)
+
+
+
+/*
+const numeros = [10, 20, 30, 40]
+
+for (let numero of numeros){
+    if (numero > 10) {
+        console.log(`${numero} é maior que 10`)
+    }else{
+        console.log(`${numero} não é maior que 10`)
+    }
+}
+    */
