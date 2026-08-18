e9b7a0a07b5fda7aa5b31aad380c596054b9c27c
KawaaaXT <kdelima8513@gmail.com>
2026-08-18 20:28:18 +0100

    Adiciona exercicio de logica 002, e 003

---
+++ secao 3 - logica de programacao/Js-Js/exercicioLogica003.js
@@ -0,0 +1,14 @@
+
+
+const FizzBuzz = numero => {
+    if (numero % 3 === 0 && numero % 5 === 0 ){
+        return "FizzBuzz"
+    }else if((numero % 3 === 0)){
+        return "Fizz"
+    }else if (numero % 5 === 0){
+        return "Buzz"
+    }else{
+        return numero
+    }
+}
+console.log(FizzBuzz(15))
---
+++ secao 3 - logica de programacao/Js-Js/exercicioLogica02.js
@@ -0,0 +1,7 @@
+
+
+const ePaisagem = (largura, altura) => largura > altura
+        
+
+
+console.log(ePaisagem(1920, 1080))
