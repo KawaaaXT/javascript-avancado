e64538ff008aa2c317b762fd0a61adc1ad7bc592
Kawaa Pereira <kdelima8513@gmail.com>
2026-09-07 22:01:33 +0100

    Adiciona revisão de arrays

---
+++ secao - 5 arrays (avancado)/java-script/revisaoarray.js
@@ -0,0 +1,29 @@
+
+/*
+const nomes = ["kaua", "ramon", "jamille"];
+
+const novoArray = [...nomes] // Rest Operator.
+
+novoArray.pop()
+console.log(nomes)
+console.log(novoArray)
+
+
+const nomes = ["kaua", "ramon", "jamille"];
+const removidos = nomes.pop()
+const primeiroRemovido = nomes.shift()
+console.log(nomes, removidos, primeiroRemovido, nomes[0])
+
+
+// convertendo string para array
+
+const nome = 'Kaua De Lima Pereira'
+const nomes = nome.split(' ')
+console.log(nomes)
+
+*/
+
+// convertendo array para string
+const nomes = ["Deyvid", "Kauã", "Roberta", "João"]
+const nome = nomes.join(' ')
+console.log(nome)
